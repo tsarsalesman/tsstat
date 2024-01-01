cc3abe45b770b41ccdbe53264faf9e7b3b3d081c
tsarsalesman <77937076+tsarsalesman@users.noreply.github.com>
2023-12-31 20:13:04 -0500

    Update utils.py

--- src/tsstat/utils.py
+++ src/tsstat/utils.py
@@ -1,11 +1,16 @@
 """
-    Utilities
-    ---------
+Utilities
+---------
 
 
 
 """
 
-
-def qSort(X:list):
-
+def qsort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        left = [x for x in arr[1:] if x < pivot]
+        right = [x for x in arr[1:] if x >= pivot]
+        return qsort(left) + [pivot] + qsort(right)
