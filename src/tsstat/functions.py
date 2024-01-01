b4d0a4ca0c3b14090bc4a6bdf18a40d9e0fe0517
tsarsalesman <77937076+tsarsalesman@users.noreply.github.com>
2023-12-31 21:10:48 -0500

    Update functions.py

--- src/tsstat/functions.py
+++ src/tsstat/functions.py
@@ -1,24 +1,41 @@
 """
-    Functions Component
-    -------------------
+Functions Component
+-------------------
 
 
 """
-from utils import qSort
+from utils import qsort
 
-def mode (values):
-    freq= {}
+def mode (data):
+    """
+    Calculates the mode of a dataset.
 
-    for i in values
+    Params
+    ------
+    data : list or tuple or set
+        The dataset.
+
+    Returns
+    -------
+    float
+        The mode value.
+    """
+    max, mode = 0
+    for val in data:
+        cnt = data.count(val)
+        if (cnt > max):
+            max = cnt
+            mode = val
+    return mode
 
 
-def mean (values): 
+def mean (data): 
     """
     Calculates the mean of a dataset.
 
     Params
     ------
-    values : list or tuple
+    data : list or tuple or set
         The dataset.
 
     Returns
@@ -26,14 +43,31 @@ def mean (values):
     float
         The mean value.
     """
-    return sum(values) / len(values)
+    return sum(data) / len(data)
 
-def median (values):
-    N = len(values)
+def median (data):
+    """
+    Calculates the median of a dataset.
+
+    Params
+    ------
+    data : list or tuple or set
+        The dataset.
+
+    Returns
+    -------
+    float
+        The median value.
+    """
+    n=len(data)
+    qsort(data)
+    if n%2==0: return((data[n//2])+(data[n//2-1]))/2
+    return data[n//2]
 
 def sumsqr (values):
     """
-    Calculates sum of squares of a dataset.
+    Calculates sum of squares of a dataset. Forgoes the definitional formula
+    because it's not great for large datasets or an odd mean.
     
     Params
     ------
@@ -50,13 +84,13 @@ def sumsqr (values):
         sqrs.append(val**2)
     return sum(sqrs) - ((sum(values))**2)/len(values)
 
-def var (values, sample=False):
+def var (data, sample=False):
     """
     Calculates the variance of a dataset.
 
     Params
     ------
-    values : list or tuple
+    data : list or tuple or set
         The dataset.
     sample : bool
         When True will calculate the sample variance,
@@ -69,4 +103,35 @@ def var (values, sample=False):
         The variance of the dataset.
     """
     _sumsqr = sumsqr(values)
-    if size == "sample"
+    if sample==True: return _sumsqr/(len(data)-1)
+    return _sumsqr/len(data)
+
+def stdev (data, sample=False):
+    """
+    Calculates the standard deviation of a dataset.
+
+    Params
+    ------
+    data : list or tuple or set
+        The dataset.
+    sample : bool
+        When True will calculate the sample variance,
+        When False (default) will calculate variance for population size.
+
+    Returns
+    -------
+    float
+        The standard deviation of the dataset.
+    float : optional
+        The variance of the dataset.
+    """
+    _sumsqr = sumsqr(values)
+    if sample==True: var = _sumsqr/(len(data)-1)
+    else: var = _sumsqr/len(data)
+    return (var)**0.5, var
+
+def sterror(data, sample=False):
+    return stdev(data, sample) / (len(data)**0.5
+
+def coeffvar(data, sample=False):
+    return (stdev(data, sample) / mean(data))*100
