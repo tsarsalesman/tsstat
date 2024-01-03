f5026afd204299a7f24b6b43304c299a1a9cc8f6
tsarsalesman <77937076+tsarsalesman@users.noreply.github.com>
2024-01-03 15:41:34 -0500

    Update utils.py

--- src/tsstat/utils.py
+++ src/tsstat/utils.py
@@ -14,3 +14,23 @@ def qsort(arr):
         left = [x for x in arr[1:] if x < pivot]
         right = [x for x in arr[1:] if x >= pivot]
         return qsort(left) + [pivot] + qsort(right)
+
+def diff(A, B):
+    """Finds the difference between two given lists of ints.
+
+    Params
+    ------
+    A : array_like
+        An array of integers
+    B : array_like
+        An array of integers
+
+    Returns
+    -------
+    Difference : array_like
+        The difference between each associated value.
+    """
+    diffs = []
+    for i in range(len(A)):
+        diffs.append(A[i] - B[i])
+    return diffs
